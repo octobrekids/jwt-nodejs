985592742949f28ebd501f5d28a4c09424f32e8c
octobrekids <chnrd.k@gmail.com>
2021-03-17 00:43:18 +0700

    Try get request

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const posts = [
+  {
+    username: "Kyle",
+    title: "Post 1",
+  },
+  {
+    username: "Jimmy",
+    title: "Post 2",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.listen(3000);
