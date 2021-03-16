da35c514e72a735ebcaccf2ebd9167aacc2f0f8d
octobrekids <chnrd.k@gmail.com>
2021-03-17 01:50:48 +0700

    refactor to authServer and other server

--- server.js
+++ server.js
@@ -23,15 +23,6 @@ app.get("/posts", authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name));
 });
 
-app.post("/login", (req, res) => {
-  // Authenticated user
-  const username = req.body.username;
-  const user = { name: username };
-
-  const accessToken = jwt.sign(user, "" + process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken: accessToken });
-});
-
 function authenticateToken(req,res,next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
@@ -47,5 +38,7 @@ function authenticateToken(req,res,next) {
         next()
     })
 }
+
+
 
 app.listen(3000);
