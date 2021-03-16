f5b70997f7aa3d6daa5d45ae55e5c5b3ac324ef0
octobrekids <chnrd.k@gmail.com>
2021-03-17 01:48:29 +0700

    test 2 server

---
+++ server2.js
@@ -0,0 +1,51 @@
+require('dotenv').config
+
+const express = require("express");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+
+// the application actually use json, make sure express server can handle it
+app.use(express.json());
+
+const posts = [
+  {
+    username: "Kyle",
+    title: "Post 1",
+  },
+  {
+    username: "Jimmy",
+    title: "Post 2",
+  },
+];
+
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name));
+});
+
+app.post("/login", (req, res) => {
+  // Authenticated user
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, "" + process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+function authenticateToken(req,res,next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    // they haven't send a token to us
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token,"" + process.env.ACCESS_TOKEN_SECRET, (err,user) => {
+        // token no longer valid
+        console.log(err)
+        if(err) return res.sendStatus(403) 
+
+        req.user = user
+        next()
+    })
+}
+
+app.listen(4000);
