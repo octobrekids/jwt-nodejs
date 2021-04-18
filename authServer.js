a686ca5b6dd84569b630dbca2ad287f699fcf1d6
octobrekids <chnrd.k@gmail.com>
2021-04-19 01:01:59 +0700

    add cors

--- authServer.js
+++ authServer.js
@@ -1,51 +1,53 @@
-require("dotenv").config;
+require('dotenv').config;
 
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 // the application actually use json, make sure express server can handle it
 app.use(express.json());
 
 let refreshTokens = [];
 
-app.delete('/logout', (req,res) => {
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-  // successful delete
-  res.sendStatus(204)
-})
-
-app.post("/token", (req, res) => {
-  const refreshToken = req.body.token;
-  if (refreshToken == null) return res.sendStatus(401);
-  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-  jwt.verify(
-    refreshToken,
-    "" + process.env.REFRESH_TOKEN_SECRET,
-    (err, user) => {
-      if (err) return res.sendStatus(403);
-      const accessToken = generateAccessToken({ name: user.name });
-      res.json({ accessToken: accessToken });
-    }
-  );
+app.delete('/logout', (req, res) => {
+	refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+	// successful delete
+	res.sendStatus(204);
 });
 
-app.post("/login", (req, res) => {
-  // Authenticated user
-  const username = req.body.username;
-  const user = { name: username };
+app.post('/token', (req, res) => {
+	const refreshToken = req.body.token;
+	if (refreshToken == null) return res.sendStatus(401);
+	if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+	jwt.verify(
+		refreshToken,
+		'' + process.env.REFRESH_TOKEN_SECRET,
+		(err, user) => {
+			if (err) return res.sendStatus(403);
+			const accessToken = generateAccessToken({ name: user.name });
+			res.json({ accessToken: accessToken });
+		}
+	);
+});
 
-  const accessToken = generateAccessToken(user);
-  const refreshToken = jwt.sign(user, "" + process.env.REFRESH_TOKEN_SECRET);
-  refreshTokens.push(refreshToken);
-  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+app.post('/login', (req, res) => {
+	// Authenticated user
+	const username = req.body.username;
+	const user = { name: username };
+
+	const accessToken = generateAccessToken(user);
+	const refreshToken = jwt.sign(user, '' + process.env.REFRESH_TOKEN_SECRET);
+	refreshTokens.push(refreshToken);
+	res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
 function generateAccessToken(user) {
-  return jwt.sign(user, "" + process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15s",
-  });
+	return jwt.sign(user, '' + process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: '15s',
+	});
 }
 
 app.listen(4000);
